c1fc41ccd61baec898deef9294c510b8892cc57c
hideto1993 <73929175+hideto1993@users.noreply.github.com>
2020-12-08 18:15:39 +0900

    ユーザー管理機能3

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,25 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  FORMAT_ZENKAKU = /\A[ぁ-んァ-ン一-龥]/.freeze
-  FORMAT_KANA = /\A[ァ-ヶ一-]+\z/.freeze
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
 
   #password
   validates_format_of :password, with: PASSWORD_REGEX, message: 'は文字と数字の両方を含めて設定してください'
 
   with_options presence: true do
-    #nickname
-    validates :nickname
-    #second_name
-    validates :second_name, format: { with: FORMAT_ZENKAKU, message: 'は全角文字で入力してください' }
-    #first_name
-    validates :first_name, format: { with: FORMAT_ZENKAKU, message: 'は全角文字で入力してください' }
-    #second_name_kana
-    validates :second_name_kana, format: { with: FORMAT_KANA, message: 'は全角カタカナで入力してください' }
-    #first_name_kana
-    validates :first_name_kana, format: { with: FORMAT_KANA, message: 'は全角カタカナで入力してください' }
     #birthday
     validates :birthday
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: '全角文字で入力してください' } do
+    validates :first_name
+    validates :second_name
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ一-]+\z/, message: '全角カタカナで入力してください' } do
+    validates :first_name_kana
+    validates :second_name_kana
   end
 end
