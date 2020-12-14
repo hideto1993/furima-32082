d6d4440d3bad0b4d97487f3fc209a1a6d220ea99
hideto1993 <73929175+hideto1993@users.noreply.github.com>
2020-12-14 20:25:09 +0900

    商品詳細表示1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_item, only: [:edit, :show]
 
   def index
     @items = Item.all.includes(:user).order('created_at DESC')
@@ -18,10 +19,23 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    unless current_user == @item.user
+      redirect_to root_path
+    end  
+  end
+
+  def show
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:title,:explanation,:category_id,:status_id,:burden_fee_id,:prefecture_id,:shipping_date_and_time_id, :price, :content, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
